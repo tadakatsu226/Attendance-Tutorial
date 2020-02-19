0d41c32f6dc3ea98d2f9bdbf26c6b5e89980e57e
tadakatsu kato <weishengjiateng@gmail.com>
2020-02-19 15:46:10 +0000

    Finish top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'staticpages/top'
+  root 'staticpages#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
