ed4fdce9d8167fa0f293a97d862d49a42d7327e3
tadakatsu kato <weishengjiateng@gmail.com>
2020-02-23 14:05:29 +0000

    Finish signup-user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'staticpages#top'
   get '/signup', to: 'users#new'
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
